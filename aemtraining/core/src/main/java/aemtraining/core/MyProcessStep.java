20ecb4a98a3709676cd2656a0dbaf68215abdb28
RohitNegi02 <rohitnegi550@gmail.com>
2018-05-30 16:36:00 +0200

    e

--- aemtraining/core/src/main/java/aemtraining/core/MyProcessStep.java
+++ aemtraining/core/src/main/java/aemtraining/core/MyProcessStep.java
@@ -21,7 +21,7 @@ import javax.jcr.Session;
 /**
  * Sample workflow process that sets an <code>approve</code> property to the payload based on the process argument value.
  */
-@Component(immediate=true,service = WorkflowProcess.class, property = {"chooser.label=Sample Workflow process"})
+@Component(immediate=true,service = WorkflowProcess.class)
 @Service
 public class MyProcessStep implements WorkflowProcess {
  
